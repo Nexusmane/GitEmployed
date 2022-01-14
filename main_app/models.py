1afba9364b53320a34ce3d06fca64695844048f8
Ernest Bruno <erndobruno@gmail.com>
2022-01-14 14:52:57 -0800

    Add a model for a Job App and create a migration afterwards. Also registers the model in admin.

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,25 @@
 from django.db import models
+from django.urls import reverse
+from datetime import date
 from django.contrib.auth.models import User
 
 # Create your models here.
+class JobApp(models.Model):
+    company = models.CharField(max_length=100)
+    job_title = models.CharField(max_length=50)
+    submission_date = models.DateField('Submission Date')
+    follow_up_date = models.DateField('Follow-Up Date')
+    job_post_url = models.CharField(max_length=999)
+    description = models.CharField(max_length=256)
+    notes = models.CharField(max_length=256)
+    excitement_level = models.IntegerField()
+    resume_url = models.CharField(max_length=999)
+    cover_letter_url = models.CharField(max_length=999)
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"App for {self.company}"
+
+    def get_absolute_url(self):
+        return reverse('app_details', kwargs={'app.id': self.id})
